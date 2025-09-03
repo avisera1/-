b8bd483ac59e983b2e857b7b985ae972ef131f21
avisera1 <gulintento@gmail.com>
2025-09-03 22:54:47 +0900

    fist コミット

---
+++ android/app/src/main/kotlin/com/example/flutter_aic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_aic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Training',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center (
+        child: ElevatedButton(
+         onPressed:(){
+            print("現在工事中です。ご不便をおかけして申し訳ございません。");
+          },
+          child: Text("詳細情報はこちら"),
+        ),
+      ),
+    );
+  }
+}
